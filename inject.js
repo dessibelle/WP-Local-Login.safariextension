ffc708d963b7fc8f23203f57a8d9f581b5dc09c9
Simon Fransson <simon@dessibelle.se>
2013-09-09 16:18:07 +0200

    Added a 'Login to localhost (http)…' context menu item, in order to redirect the form to a non-https server for sites running https.

--- inject.js
+++ inject.js
@@ -1,14 +1,19 @@
-function wpLocalLoginRedirect() {
+function wpLocalLoginRedirect(force_http) {
 
     var form = document.getElementById('loginform');
-    
+
     if (typeof form !== 'undefined')
-    {    
-        var url = form.getAttribute('action');        
-        url = url.replace(/(:\/\/)(.[^\/]+)/i, '$1localhost');
-        
+    {
+        var url = form.getAttribute('action');
+
+        if (force_http) {
+            url = url.replace(/(http[s]?)(:\/\/)(.[^\/]+)/i, 'http$2localhost');
+        } else {
+            url = url.replace(/(:\/\/)(.[^\/]+)/i, '$1localhost');
+        }
+
         form.setAttribute('action', url);
-        
+
         form.submit();
     }
 }
@@ -16,29 +21,31 @@ function wpLocalLoginRedirect() {
 function handleMessage(msgEvent) {
     var messageName = msgEvent.name;
     var messageData = msgEvent.message;
-        
-    if (messageName === "redirect") {
-        wpLocalLoginRedirect();
+
+    var force_http = messageName == 'redirect_http';
+
+    if (messageName === "redirect" || messageName === "redirect_http") {
+        wpLocalLoginRedirect(force_http);
     }
 }
 
 function validateContextMenu() {
-    
+
     if (window.location.pathname) {
         var matches = window.location.pathname.match(/.*\/wp-login\.php.*/i);
-        
+
         if (matches)
             return matches.length > 0;
     }
-    
-    
+
+
     return false;
 }
 
 function onContextMenu(ev) {
     var UserInfo = {};
     UserInfo.showMenuItem = validateContextMenu();
-        
+
     safari.self.tab.setContextMenuEventUserInfo(ev, UserInfo);
 };
 
